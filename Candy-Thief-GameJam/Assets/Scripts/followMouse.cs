b939e81dfe03e4e876ce12505363bc85a04f8979
nathanRainieriArt <nathan.rainierigit@gmail.com>
2025-11-22 18:11:38 -0300

    Slap Animation Upload

--- Candy-Thief-GameJam/Assets/Scripts/followMouse.cs
+++ Candy-Thief-GameJam/Assets/Scripts/followMouse.cs
@@ -7,8 +7,31 @@ public class followMouse : MonoBehaviour
     Vector3 pos;
     public float positionOffset = 3f;
     public Vector3 modelOffset;
+    [SerializeField] private Animator slapAnim;
+    
+    [SerializeField] private string slapAnimStr = "slapAnim";
+
+    void Strart()
+    {
+        
+    }
     void Update()
     {
+       // if (mAnimator != null)
+        //{
+            
+            if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            slapAnim.Play(slapAnimStr, 0 , 0.0f);
+        }
+        /*
+            {
+                
+                
+            }*/
+
+        //}
+
         pos = Input.mousePosition;
         pos.z = positionOffset;
         Vector3 worldPos = Camera.main.ScreenToWorldPoint(pos);
