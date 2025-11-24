dec68c1b1350f260ba0c2f451fda96d056210aea
PurpleBeeStudio <gaelq.lacerda@gmail.com>
2025-11-24 19:40:13 -0300

    Win menu Implementado

--- Candy-Thief-GameJam/Assets/Scripts/GameplayScripts/cliquesVitoria.cs
+++ Candy-Thief-GameJam/Assets/Scripts/GameplayScripts/cliquesVitoria.cs
@@ -1,9 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class cliquesVitoria : MonoBehaviour
 {
-    [Header("Configura��o da Fase: Tapas")]
+    [Header("Configuração da Fase: Tapas")]
     public int cliquesNecessarios = 5; //n� de cliques para terminar a fase; configur�vel
 
     [Header("UI")]
@@ -14,7 +17,7 @@ public class cliquesVitoria : MonoBehaviour
 
     private void Update()
     {
-        textoCliques.text = $"Tapas: {cliquesAtuais}/{cliquesNecessarios}";
+        textoCliques.text = $"Slaps: {cliquesAtuais}/{cliquesNecessarios}";
     }
     //m�todo que � chamado toda vez que o ladr�o for clicado
     public void RegistrarClique()
@@ -22,7 +25,7 @@ public class cliquesVitoria : MonoBehaviour
         cliquesAtuais++;
 
         //atualiza os cliques na tela
-        textoCliques.text = $"Tapas: {cliquesAtuais}/{cliquesNecessarios}";
+        textoCliques.text = $"Slaps: {cliquesAtuais}/{cliquesNecessarios}";
 
         //verifica se a fase foi conclu�da
         if (cliquesAtuais >= cliquesNecessarios)
@@ -33,7 +36,6 @@ public class cliquesVitoria : MonoBehaviour
     private void faseConcluida()
     {
         Debug.Log("Fase Concluída!");
-        textoCliques.text = "Fase concluída!";
-        //NOTA: fazer transi��o de fase depois
+        SceneManager.LoadScene("WinMenu");
     }
 }
--- Candy-Thief-GameJam/Assets/Scripts/MenuScripts/EndMenu.cs
+++ Candy-Thief-GameJam/Assets/Scripts/MenuScripts/EndMenu.cs
@@ -1,10 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class EndMenu : MonoBehaviour
 {
+
+    private void Start()
+    {
+        Cursor.visible = true;
+    }
     public void ClickReplay()
     {
         SceneManager.LoadScene("Game");
